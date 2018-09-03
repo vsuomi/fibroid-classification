8b59ab676aff1ca7053a9500dc162397b889142d
cimppact-turku\visa <visa.suomi@gmail.com>
2018-09-03 11:26:42 +0300

    Scaling features with zero standard deviation now outputs zero instead of NaN

--- scale_features.py
+++ scale_features.py
@@ -10,13 +10,14 @@ Created on Thu May 31 11:38:48 2018
     
 @description:
     
-    This function is used to scale features using standard deviation
+    This function is used to scale features using different scaling types
     
 '''
 
 #%% import necessary packages
 
 import numpy as np
+import pandas as pd
 
 #%% define function
 
@@ -29,20 +30,27 @@ def scale_features(features, scaling):
         scaling: type of scaling: linear ('linear'), logarithmic ('log') or
         z-score ('z-score')
     Returns:
-        scaled_features: scaled features with zero mean and unit variance
+        scaled_features: scaled features
     '''
     
     if scaling == 'linear':
         min_val = features.min()
         max_val = features.max()
         scale = (max_val - min_val) / 2.0
-        scaled_features = (features - min_val) / scale - 1.0
+        a = (features - min_val)
+        b = scale
+        scaled_features = np.divide(a, b, out=np.zeros_like(a), where=b!=0) - 1.0   # NaN to zero - 1
     elif scaling == 'log':
         scaled_features = np.log(features + 1.0)
     elif scaling == 'z-score':
-        scaled_features = (features - features.mean()) / features.std()
+        a = (features - features.mean())
+        b = features.std()
+        scaled_features = np.divide(a, b, out=np.zeros_like(a), where=b!=0)     # NaN to zero
     else:
         print('Unknown scaling type')
         scaled_features = features
+        
+    scaled_features = pd.DataFrame(scaled_features, columns = list(features), 
+                                            index = features.index, dtype = float)
     
     return scaled_features
