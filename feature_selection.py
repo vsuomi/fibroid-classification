6e236be5d6a0529f457cec2e9ce98dd54aa9918b
cimppact-turku\visa <visa.suomi@gmail.com>
2019-06-07 13:00:28 +0300

    Changes to plotting parameters

--- feature_selection.py
+++ feature_selection.py
@@ -675,10 +675,10 @@ plt.xlabel('Gamma')
 
 # plot correlations
 
-f9 = plt.figure(figsize = (6, 6))
+f9 = plt.figure(figsize = (4, 4))
 ax = sns.heatmap(feature_corr, mask = feature_corr_mask, cmap = cmap, vmin = -1, vmax = 1, center = 0,
-                 square = True, linewidths = 0.5, cbar_kws = {'shrink': 0.5, 'ticks': [-1, 0, 1],
-                                                              'pad': -0.11})
+                 square = True, linewidths = 0.5, cbar_kws = {'shrink': 0.6, 'ticks': [-1, 0, 1],
+                                                              'pad': 0})
 
 f10 = plt.figure(figsize = (6, 6))
 ax = sns.heatmap(method_corr, mask = method_corr_mask, cmap = cmap, vmin = -1, vmax = 1, center = 0,
